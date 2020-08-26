01b9c3f84d3ebd62cc1fd8bcc63f5caf1eff47e5
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-08-26 14:32:34 -0300

    Cliente class renamed

--- DAO/src/dev/Cliente.java
+++ DAO/src/dev/Cliente.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Select {
+public class Cliente {
 
 	public static void main(String[] args) {
 		
