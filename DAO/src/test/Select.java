9552754ec3825aaf585fa7f11ebe41f4d23985c0
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-08-25 22:24:56 -0300

    initial commit DAO

---
+++ DAO/src/test/Select.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Select {
+
+	public static void main(String[] args) {
+		
+		try {
+			Connection conn = MySQLDB.getInstance().getConnection();
+			conn.setAutoCommit(false);
+			String select = "SELECT * FROM persona";
+			PreparedStatement ps = conn.prepareStatement(select);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				System.out.print(rs.getInt(1));
+				System.out.print(", ");
+				System.out.print(rs.getString(2));
+				System.out.print(", ");
+				System.out.print(rs.getInt(3));
+				System.out.println();
+			}
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();	
+		}
+
+	}
+
+}
